edd9df7c1ef0abaf2f3c333d189ce00ca9712341
Atopos <1827235572@qq.com>
2025-06-30 15:12:34 +0800

    Update index.js url

--- static/index.js
+++ static/index.js
@@ -14,7 +14,7 @@ let __Time = 20;
 let __k = 4;
 let _close = false;
 let _fsj = false;
-var url = 'image/ClickBefore.png';
+var url = 'static/image/ClickBefore.png';
 
 function isplaying() {
     return document.getElementById('welcome').style.display == 'none' &&
